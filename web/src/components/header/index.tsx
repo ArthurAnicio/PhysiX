9360843604006e2f4bf21ac1e31a30da38600503
NotCclo <marcelosg909@gmail.com>
2024-05-07 19:05:08 -0300

    Header

--- web/src/components/header/index.tsx
+++ web/src/components/header/index.tsx
@@ -5,17 +5,21 @@ import { Link } from "react-router-dom";
 let mainLogo = require("../../assets/images/imgs/grandeClaroPng.png");
 
 interface HeaderParam extends InputHTMLAttributes<HTMLInputElement> {
-    path: string;
-    title: string;
+  path: string;
+  title: string;
 }
 
-const Header: React.FC<HeaderParam> = ({path, title}) => { 
-    return(
-        <div id="headerDefault">
-            <Link to={path}><img src={mainLogo} id="headerLogo"/></Link>
-            <h1>{title}</h1>
-            <AtomSVG id={"atomHeader"}/>
-        </div>
-    )
-}
+const Header: React.FC<HeaderParam> = ({ path, title }) => {
+  return (
+    <div id="headerDefault">
+      <Link to={path}  id="headerLogo" >
+        <img src={mainLogo}/>
+      </Link>
+      <h1>{title}</h1>
+      <div id="atomContainer">
+        <AtomSVG id={"atomHeader"} />
+      </div>
+    </div>
+  );
+};
 export default Header;
