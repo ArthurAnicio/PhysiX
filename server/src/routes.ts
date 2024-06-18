523e94d520b0dad1c1ae544e336d16d6eb1ca8d6
Arthur Morais <arthur.morais@facode.com.br>
2024-06-18 18:08:23 -0300

    classController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import UserController from './controllers/UserController';
 import TeacherController from './controllers/TeacherController';
+import ClassController from './controllers/ClassController';
 
 const routes = express.Router();
 const userController = new UserController();
 const teacherController = new TeacherController();
+const classController = new ClassController();
 
 routes.get('/teacher', teacherController.index);
 routes.post('/teacher', teacherController.create)
@@ -16,5 +18,8 @@ routes.post('/user', userController.create)
 routes.post('/forgot-password', userController.forgotPass)
 routes.post('/reset-pass',userController.passReset)
 routes.get('/getuser',userController.getUser)
+
+routes.get('/class', classController.index)
+routes.post('/class', classController.create)
 
 export default routes;
