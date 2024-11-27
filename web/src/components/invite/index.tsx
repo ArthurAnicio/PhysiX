083532a380d28f72f8e7e1762427122141d8f1d3
BannedCclo <cecelokun@gmail.com>
2024-11-27 17:38:31 -0300

    invite filtered fix

--- web/src/components/invite/index.tsx
+++ web/src/components/invite/index.tsx
@@ -1,125 +1,160 @@
-import styles from './invite.module.css';
-import api from '../../services/api';
-import { InputHTMLAttributes, useState, useEffect } from 'react';
+import styles from "./invite.module.css";
+import api from "../../services/api";
+import { InputHTMLAttributes, useState, useEffect } from "react";
 
-interface Invite{
-    id: number;
-    user_id: number;
-    teacher_id: number;
-    accepted: boolean;
-    schedule: Schedule;
+interface Invite {
+  id: number;
+  user_id: number;
+  teacher_id: number;
+  accepted: boolean;
+  schedule: string;
 }
 
-interface InviteProps extends InputHTMLAttributes<HTMLInputElement>{
-    invite: Invite;
-    sync: () => void;
+interface InviteProps extends InputHTMLAttributes<HTMLInputElement> {
+  invite: Invite;
+  sync: () => void;
 }
 
-interface Schedule{
-    week_day: string;
-    from: string;
-    to: string;
-    id: number;
+interface Schedule {
+  week_day: string;
+  from: string;
+  to: string;
+  id: number;
 }
 
 const Invite: React.FC<InviteProps> = ({ invite, sync }) => {
-    
-    const [user, setUser] = useState({id: 0,name:'',avatar:''})
-    const [avatar, setAvatar] = useState('')
-    const [schedule,setSchedule] = useState<Schedule>(invite.schedule);
-    
-    useEffect(() => {
-        getUser();
-    }, []);
+  const [user, setUser] = useState({ id: 0, name: "", avatar: "" });
+  const [avatar, setAvatar] = useState("");
+  const [schedule, setSchedule] = useState<Schedule>(
+    JSON.parse(invite.schedule)
+  );
+  useEffect(() => {
+    getUser();
+  }, []);
 
-    useEffect(()=>{
-        if (user.avatar){
-            getAvatar(user.avatar);
-        }
-    }, [user.avatar])
-
-    useEffect(()=>{
-        formatSchedule()
-    },[invite.schedule])
-
-    async function getUser() {
-        const response = await api.get(`/getuser?id=${invite.user_id}`);
-        if (response.status === 200) {
-            setUser(response.data);
-        }
+  useEffect(() => {
+    if (user.avatar) {
+      getAvatar(user.avatar);
     }
+  }, [user.avatar]);
 
-    async function getAvatar(avatarPath: string) {
-        try {
-          const response = await api.get("/avatar", {
-            params: { route: avatarPath },
-          });
-          if (response.status === 200) {
-            setAvatar(response.request.responseURL);
-          } else {
-            alert("Falha no avatar!");
-            console.log(response);
-          }
-        } catch (err) {
-          alert("Falha no avatar!");
-          console.log(err);
-        }
+  useEffect(() => {
+    formatSchedule();
+  }, []);
+
+  async function getUser() {
+    const response = await api.get(`/getuser?id=${invite.user_id}`);
+    if (response.status === 200) {
+      setUser(response.data);
     }
+  }
 
-    function formatSchedule(){
-        switch (schedule.week_day){
-            case '0':
-                return setSchedule({week_day:'Domingo', from: schedule.from, to: schedule.to,id: schedule.id});
-            case '1':
-                return setSchedule({week_day:'Segunda', from: schedule.from, to: schedule.to,id: schedule.id});
-            case '2':
-                return setSchedule({week_day:'Terça', from: schedule.from, to: schedule.to,id: schedule.id});
-            case '3':
-                return setSchedule({week_day:'Quarta', from: schedule.from, to: schedule.to,id: schedule.id});
-            case '4':
-                return setSchedule({week_day:'Quinta', from: schedule.from, to: schedule.to,id: schedule.id});
-            case '5':
-                return setSchedule({week_day:'Sexta', from: schedule.from, to: schedule.to,id: schedule.id});
-            case '6':
-                return setSchedule({week_day:'Sábado', from: schedule.from, to: schedule.to,id: schedule.id});
-            default:
-                return '';
-        }
+  async function getAvatar(avatarPath: string) {
+    try {
+      const response = await api.get("/avatar", {
+        params: { route: avatarPath },
+      });
+      if (response.status === 200) {
+        setAvatar(response.request.responseURL);
+      } else {
+        alert("Falha no avatar!");
+        console.log(response);
+      }
+    } catch (err) {
+      alert("Falha no avatar!");
+      console.log(err);
     }
+  }
 
-    async function refuse(){
-        const response = await api.delete(`/invite?id=${invite.id}`);
-        if (response.status === 200){
-            sync()
-        }else{
-            alert('Falha ao recusar convite.');
-            console.log(response);
-        }
+  function formatSchedule() {
+    switch (schedule.week_day) {
+      case "0":
+        return setSchedule({
+          week_day: "Domingo",
+          from: schedule.from,
+          to: schedule.to,
+          id: schedule.id,
+        });
+      case "1":
+        return setSchedule({
+          week_day: "Segunda",
+          from: schedule.from,
+          to: schedule.to,
+          id: schedule.id,
+        });
+      case "2":
+        return setSchedule({
+          week_day: "Terça",
+          from: schedule.from,
+          to: schedule.to,
+          id: schedule.id,
+        });
+      case "3":
+        return setSchedule({
+          week_day: "Quarta",
+          from: schedule.from,
+          to: schedule.to,
+          id: schedule.id,
+        });
+      case "4":
+        return setSchedule({
+          week_day: "Quinta",
+          from: schedule.from,
+          to: schedule.to,
+          id: schedule.id,
+        });
+      case "5":
+        return setSchedule({
+          week_day: "Sexta",
+          from: schedule.from,
+          to: schedule.to,
+          id: schedule.id,
+        });
+      case "6":
+        return setSchedule({
+          week_day: "Sábado",
+          from: schedule.from,
+          to: schedule.to,
+          id: schedule.id,
+        });
+      default:
+        return "";
     }
+  }
 
-    return (
-        <div className={styles.invite}>
-            <div className={styles.perfil}>
-                <img  className={styles.foto} src={avatar} alt="" />
-                <div className={styles.nome}>{user.name}</div>
-            </div>
-            <div className={styles.agenda}>
-                <label className={styles.diaSemana}>{schedule.week_day}</label>
-                <div className={styles.horario}>
-                    <label>De:  {schedule.from}</label>
-                    <label>Até: {schedule.to}</label>
-                </div>
-            </div>
-            <div className={styles.botoes}>
-                <div className={styles.aceitar}>
-                    <i className="fa-solid fa-check"></i>
-                </div>
-                <div className={styles.recusar} onClick={refuse}>
-                    <i className="fa-solid fa-times"></i>
-                </div>
-            </div>
+  async function refuse() {
+    const response = await api.delete(`/invite?id=${invite.id}`);
+    if (response.status === 200) {
+      sync();
+    } else {
+      alert("Falha ao recusar convite.");
+      console.log(response);
+    }
+  }
+
+  return (
+    <div className={styles.invite}>
+      <div className={styles.perfil}>
+        <img className={styles.foto} src={avatar} alt="" />
+        <div className={styles.nome}>{user.name}</div>
+      </div>
+      <div className={styles.agenda}>
+        <label className={styles.diaSemana}>{schedule.week_day}</label>
+        <div className={styles.horario}>
+          <label>De: {schedule.from}</label>
+          <label>Até: {schedule.to}</label>
         </div>
-    );
-}
+      </div>
+      <div className={styles.botoes}>
+        <div className={styles.aceitar}>
+          <i className="fa-solid fa-check"></i>
+        </div>
+        <div className={styles.recusar} onClick={refuse}>
+          <i className="fa-solid fa-times"></i>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Invite;
--- web/src/pages/invites/index.tsx
+++ web/src/pages/invites/index.tsx
@@ -1,97 +1,112 @@
-import styles from "./Invites.module.css"
-import Header from '../../components/header';
+import styles from "./Invites.module.css";
+import Header from "../../components/header";
 import Footer from "../../components/footer";
 import Invite from "../../components/invite";
-import React, { useState, useEffect, FormEvent } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, FormEvent } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import api from "../../services/api";
 
-interface Schedule{
-    week_day: string;
-    from: string;
-    to: string;
-    id: number;
+interface Schedule {
+  week_day: string;
+  from: string;
+  to: string;
+  id: number;
 }
 
-interface InviteItem{
-    id: number;
-    user_id: number;
-    teacher_id: number;
-    accepted: boolean;
-    schedule: Schedule;
+interface InviteItem {
+  id: number;
+  user_id: number;
+  teacher_id: number;
+  accepted: boolean;
+  schedule: string;
 }
 
 function Invites() {
+  const [teacher, setTeacher] = useState({
+    name: "",
+    email: "",
+    id: 0,
+    avatar: "",
+    number: "",
+    password: "",
+    schedule: null,
+  });
+  const [invites, setInvites] = useState<InviteItem[]>([]);
+  const [invitesFiltered, setInvitesFiltered] = useState<InviteItem[]>([]);
+  const [invite, setInvite] = useState<InviteItem>();
+  const location = useLocation();
+  const navigate = useNavigate();
+  const { teacherId } = location.state || { teacherId: 0 };
+  const [stateId, setStateId] = useState(teacherId);
 
-    const [teacher, setTeacher] = useState({ name: '', email: '', id: 0, avatar: '',number:'', password: '', schedule: null });
-    const [invites, setInvites] = useState<InviteItem[]>([]);
-    const [invitesFiltred, setInvitesFiltred] = useState<InviteItem[]>([])
-    const [invite, setInvite] = useState<InviteItem>({id:1,user_id:1,teacher_id:1,accepted:false,schedule:{"week_day":"1","from":"teste","to":"teste","id":0}});
-    const location = useLocation();
-    const navigate = useNavigate();
-    const { teacherId } = location.state || { teacherId: 0 };
-    const [stateId, setStateId] = useState(teacherId)
+  useEffect(() => {
+    getTeacher();
+  }, [teacherId]);
 
+  useEffect(() => {
+    fetchInvites();
+  }, []);
 
-    useEffect(() => {
-        getTeacher();
-    },[teacherId]);
+  useEffect(() => {
+    console.log(
+      invites.filter((invite: InviteItem) => invite.accepted == false)
+    );
+    setInvitesFiltered(
+      invites.filter((invite: InviteItem) => invite.accepted == false)
+    );
+  }, [invites]);
 
-    useEffect(()=>{
-        fetchInvites();
-    },[]);
+  function teste() {}
 
-    function teste(){}
-
-    async function getTeacher() {
-        try {
-            
-            const response = await api.get('/getTeacher', { params: { id: teacherId } });
-            if (response.status === 200) {
-                console.log(response.data)
-                setTeacher(response.data);
-                setStateId(response.data.id)
-            } else {
-                alert('Falha no login! Por favor tente novamente.');
-                navigate('/log_in_teacher');
-            }
-        } catch (error) {
-            alert('Falha no login!');
-            navigate('/log_in_teacher');
-            console.log(error);
-        }
+  async function getTeacher() {
+    try {
+      const response = await api.get("/getTeacher", {
+        params: { id: teacherId },
+      });
+      if (response.status === 200) {
+        console.log(response.data);
+        setTeacher(response.data);
+        setStateId(response.data.id);
+      } else {
+        alert("Falha no login! Por favor tente novamente.");
+        navigate("/log_in_teacher");
+      }
+    } catch (error) {
+      alert("Falha no login!");
+      navigate("/log_in_teacher");
+      console.log(error);
     }
+  }
 
-    async function fetchInvites(){
-        try {
-            api.get(`/invite?teacher_id=${teacherId}`).then((response)=> {
-               if (response.status === 200) {
-                    console.log(response.data)
-                    setInvites(response.data);
-                    setInvitesFiltred(invites.filter((invite: InviteItem) => invite.accepted === false));
-                } else {
-                    alert('Falha no login! Por favor tente novamente.');
-                    navigate('/log_in_teacher');
-                } 
-            });
-            
-        } catch (error) {
-            alert('Falha no login!');
-            navigate('/log_in_teacher');
-            console.log(error);
+  async function fetchInvites() {
+    try {
+      api.get(`/invite?teacher_id=${teacherId}`).then((response) => {
+        if (response.status === 200) {
+          console.log(response.data);
+          setInvites(response.data);
+        } else {
+          alert("Falha no login! Por favor tente novamente.");
+          navigate("/log_in_teacher");
         }
+      });
+    } catch (error) {
+      alert("Falha no login!");
+      navigate("/log_in_teacher");
+      console.log(error);
     }
+  }
 
-    return (
-        <div>
-            <Header state={stateId} title='Pedidos de aula'/>
-            <main className={styles.content}>
-                <Invite invite={invite} sync={teste} />
-                {invitesFiltred.map((invite: InviteItem) => <Invite key={invite.id} invite={invite} sync={fetchInvites} />)}
-            </main>
-            <Footer/>
-        </div>
-    )
+  return (
+    <div>
+      <Header state={stateId} title="Pedidos de aula" />
+      <main className={styles.content}>
+        {invitesFiltered.map((invite: InviteItem) => (
+          <Invite key={invite.id} invite={invite} sync={fetchInvites} />
+        ))}
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default Invites
+export default Invites;
