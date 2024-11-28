67373a3048c4de9de646a47d8e27a44cf0c3cf3e
BannedCclo <cecelokun@gmail.com>
2024-11-28 09:57:39 -0300

    Eu simplesmente me recuso a acreditar

--- web/src/pages/unverified_teacher/index.tsx
+++ web/src/pages/unverified_teacher/index.tsx
@@ -1,38 +1,44 @@
-import styles from './Unverified.module.css' 
-import Header from '../../components/header'
-import { useState } from 'react'
-import { useLocation } from 'react-router-dom'
-import api from '../../services/api'
-import { Link } from 'react-router-dom'
+import styles from "./Unverified.module.css";
+import Header from "../../components/header";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import api from "../../services/api";
+import { Link } from "react-router-dom";
 
 function UnverifiedTeacher() {
-    const [canReqAgain, setCanReqAgain] = useState(true)
-    const location = useLocation()
-    const {email} = location.state || ''
-    async function handleSendEmail() {
-        if (canReqAgain) {
-            try {
-                await api.post('/verify-teacher', {email}).then(() => {
-                  alert('Email re-enviado!');
-                })} catch (err) {
-                  alert('Erro ao enviar email!');
-                  console.log(err);
-                }
-            setCanReqAgain(false)
-            window.setTimeout(() => setCanReqAgain(true), 30000)
-        } else {
-            alert('Aguarde 30 segundos para enviar novamente um email.')
-        }
+  const [canReqAgain, setCanReqAgain] = useState(true);
+  const location = useLocation();
+  const { email } = location.state || "";
+  async function handleSendEmail() {
+    if (canReqAgain) {
+      try {
+        await api.post("/verify-teacher", { email }).then(() => {
+          alert("Email re-enviado!");
+        });
+      } catch (err) {
+        alert("Erro ao enviar email!");
+        console.log(err);
+      }
+      setCanReqAgain(false);
+      window.setTimeout(() => setCanReqAgain(true), 30000);
+    } else {
+      alert("Aguarde 30 segundos para enviar novamente um email.");
     }
-    return (
-        <div className={styles.unverified}>
-            <Header state={0} title=''/>
-            <h1>Você ainda não confirmou seu e-mail.</h1>
-            <p>Por favor, verifique seu e-mail para confirmar sua conta.</p>
-            <button onClick={handleSendEmail}>Clique aqui para enviar novamente o e-mail de verificação</button>
-            <Link to="/log_in_teacher">Voltar à tela de login</Link>
+  }
+  return (
+    <div className={styles.unverified}>
+      <Header state={0} title="" />
+      <div id={styles.content}>
+        <div id={styles.card}>
+          <h1>Você ainda não confirmou seu e-mail.</h1>
+          <p>Por favor, verifique seu e-mail para confirmar sua conta.</p>
+          <p>Clique aqui para enviar novamente o e-mail de verificação</p>
+          <button onClick={handleSendEmail}>Enviar</button>
+          <Link to="/log_in_teacher">Voltar à tela de login</Link>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default UnverifiedTeacher
+export default UnverifiedTeacher;
